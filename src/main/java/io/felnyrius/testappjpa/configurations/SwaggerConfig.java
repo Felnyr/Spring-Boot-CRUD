6587d98b2ed39a6c311b698eb300e4097bd98620
Felnyr <pawlello@gmail.com>
2022-12-11 01:13:34 +0100

    added swagger2 and downgraded springBoot ver for compatibility

---
+++ src/main/java/io/felnyrius/testappjpa/configurations/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package io.felnyrius.testappjpa.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
+
+// URL UI is http://localhost:8080/swagger-ui/
+// URL JSON is http://localhost:8080/v2/api-docs
