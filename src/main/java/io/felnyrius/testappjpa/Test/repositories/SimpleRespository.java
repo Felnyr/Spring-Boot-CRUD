13649d3f2d0093f350801ebd641083f24d98ea08
Felnyr <pawlello@gmail.com>
2023-02-20 20:31:00 +0100

    added custom query method to simple repository

--- src/main/java/io/felnyrius/testappjpa/Test/repositories/SimpleRespository.java
+++ src/main/java/io/felnyrius/testappjpa/Test/repositories/SimpleRespository.java
@@ -2,8 +2,12 @@ package io.felnyrius.testappjpa.Test.repositories;
 
 import io.felnyrius.testappjpa.Test.models.SimpleDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface SimpleRespository extends JpaRepository<SimpleDto, Integer> {
+    @Query(value = "SELECT * FROM simple WHERE simple.age > 5", nativeQuery = true)
+    List<SimpleDto> getAgeMoreThan5();
 }
